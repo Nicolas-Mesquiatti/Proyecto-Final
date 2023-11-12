131098e9b9edeb990a8bb17d4e811622140cd0ff
Nicolas <you@example.com>
2023-11-12 17:56:28 -0300

    Mejore lo responsive y el form

--- Pages-de-footer/Trabaja/Trabaja.js
+++ Pages-de-footer/Trabaja/Trabaja.js
@@ -23,5 +23,24 @@ const expresiones = {
 }
 
 
-let formulario = document.getElementById("form");
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${'Enviaste tu formulario correctamente, Estate atento al correo para la convocatoria, Suerte! '}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Suertee', 'success')
+  })
+}
+
 
